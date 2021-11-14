cffb5b6b8d9926c44e44057089cfcabfbcc2b89b
Lee <320011927@qq.com>
2021-11-14 17:49:52 +0800

    lxs first commit

---
+++ src/main/java/com/lxs/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.lxs.git;
+
+/**
+ * @author lixianshuai
+ * @date 2021/11/14 17:42
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
