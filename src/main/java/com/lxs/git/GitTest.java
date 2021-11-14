2a162618b368daf5d5359264a7d3ad5334bd4d86
lxshub <92366542+lxshub@users.noreply.github.com>
2021-11-14 21:34:31 +0800

    lxshub pull test

--- src/main/java/com/lxs/git/GitTest.java
+++ src/main/java/com/lxs/git/GitTest.java
@@ -15,5 +15,6 @@ public class GitTest {
         System.out.println("master!");
         System.out.println("hot-fix!");
         System.out.println("master push test!");
+        System.out.println("lxshub pull test!");
     }
 }
